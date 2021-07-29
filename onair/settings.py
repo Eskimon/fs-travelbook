cc905db9b9f5afd117c880df2fbd4148d1c084bb
Eskimon <eskimon@outlook.com>
2021-07-29 10:35:38 +0000

    Fix some config issue

--- onair/settings.py
+++ onair/settings.py
@@ -123,10 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
-
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, "dist")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -140,3 +140,15 @@ WEBPACK_LOADER = {
         "STATS_FILE": os.path.join(BASE_DIR, "webpack-stats.json"),
     }
 }
+
+
+try:
+    from .settings_dev import *  # noqa
+except ImportError:
+    print('No dev settings found, carrying on')
+
+try:
+    from .settings_prod import *  # noqa
+except ImportError:
+    print('No prod settings found, carrying on')
+
