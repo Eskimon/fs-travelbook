f957099071c950b6c704d39bf0b1466980ca3b72
Eskimon <eskimon@outlook.com>
2022-06-17 21:06:11 +0000

    Fix a bug

--- pictures/templatetags/get_item.py
+++ pictures/templatetags/get_item.py
@@ -6,4 +6,4 @@ register = template.Library()
 @register.simple_tag
 def get_selected_item(dictionary, key, needle):
     dico = dict(dictionary).get(key)
-    return "selected" if str(needle) in dico else ""
+    return "selected" if dico and (str(needle) in dico) else ""
