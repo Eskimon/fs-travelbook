98a0193e50a7f41d8836023aa48008238383b70f
Eskimon <eskimon@outlook.com>
2022-06-18 19:08:47 +0200

    Keep flight filtering when paginating

--- pictures/templatetags/get_item.py
+++ pictures/templatetags/get_item.py
@@ -7,3 +7,9 @@ register = template.Library()
 def get_selected_item(dictionary, key, needle):
     dico = dict(dictionary).get(key)
     return "selected" if dico and (str(needle) in dico) else ""
+
+
+@register.simple_tag
+def encode_flights_item(dictionary):
+    dico = dict(dictionary).get("flights", [])
+    return "&".join([f"flights={f}" for f in dico]) if dico else ""
